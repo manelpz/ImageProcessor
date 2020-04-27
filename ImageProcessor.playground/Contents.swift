9ea897dd2c35866b21a3d113d45d6f243f57b8f9
Emmanuel López Guerrero <manelpz@icloud.com>
2020-04-26 22:51:21 -0500

    InstaFilter processor - final course project

--- ImageProcessor.playground/Contents.swift
+++ ImageProcessor.playground/Contents.swift
@@ -4,49 +4,44 @@ import UIKit
 
 let image = UIImage(named: "sample")
 
-// Process the image!
-image
-
-var myRGBA = RGBAImage(image: image ?? <#default value#>)!
-
-var totalRed = 0
-var totalGreen = 0
-var totalBlue = 0
-/*
-for y in 0..<myRGBA.height{
-    for x in 0..<myRGBA.width{
-        let index = y * myRGBA.width + x
-        var pixel = myRGBA.pixels[index]
-        totalRed += Int(pixel.red)
-        totalGreen += Int(pixel.green)
-        totalBlue += Int(pixel.blue)
-    }
-}
 
 
-let count = myRGBA.width * myRGBA.height
-let avgRed = totalRed/count
-let avgGreen = totalGreen/count
-let avgBlue = totalBlue/count
-*/
 
-let avgRed = 118
-let avgGreen = 98
-let avgBlue = 83
+//filter red color
 
-for y in 0..<myRGBA.height{
-    for x in 0..<myRGBA.width{
-        let index = y * myRGBA.width + x
-        var pixel = myRGBA.pixels[index]
-        let redDiff = Int(pixel.red) - avgRed
-        
-        if(redDiff>0)
-        {
-            pixel.red = UInt8( max(0,min(255,avgRed + redDiff*5)))
-            myRGBA.pixels[index] = pixel
+func FilterImageRed() -> UIImage{
+    var myRGBA = RGBAImage(image: image ?? <#default value#>)!
+    
+    var totalRed = 0
+    let avgRed = 83
+    for y in 0..<myRGBA.height{
+        for x in 0..<myRGBA.width{
+            let index = y * myRGBA.width + x
+            var pixel = myRGBA.pixels[index]
+            let redDiff = Int(pixel.red) - avgRed
+            
+            if(redDiff>0)
+            {
+                pixel.red = UInt8( max(0,min(255,avgRed + redDiff*2)))
+                myRGBA.pixels[index] = pixel
+            }
         }
     }
+
+    return (myRGBA.toUIImage())!
 }
 
-let mewImage2 = myRGBA.toUIImage()
-image
+//scale of gray
+func FilterImageGrayScale() -> UIImage{
+    
+        // Create a new UIImage object
+    //let newImage: UIImage = UIImage(CGImage: imageRef)
+
+    
+    //return (newImage)
+}
+
+
+let imageRE = FilterImageRed()
+imageRE
+
